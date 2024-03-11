16609e0ed21441a16f01da69183344bb8cbc3c4d
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-03-11 23:35:59 +0545

    Objects && TypeAliases

---
+++ 01_basics/typeAliases.js
@@ -0,0 +1,4 @@
+function createUser(user) {
+    return { name: "Subash", email: "chysubash15963@gmail.com", isActive: true };
+}
+createUser({ name: "Subash", email: "chysubash15963@gmail.com", isActive: true });
---
+++ 01_basics/typeAliases.ts
@@ -0,0 +1,10 @@
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function createUser(user: User):User {
+          return {name : "Subash", email : "chysubash15963@gmail.com", isActive : true}
+}
+createUser({name : "Subash", email : "chysubash15963@gmail.com", isActive : true})
